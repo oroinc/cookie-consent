7e376bc70a87c979e68e0317c8f6cd045c8560aa
Andrii Yatsenko <ayatsenko@oroinc.com>
2021-01-14 20:46:41 +0000

    BAP-20330: Upgrade doctrine/orm to 2.8 - upgraded doctrine/persistence and doctrine/orm packages - upgraded jms/serializer and hwi/oauth-bundle - upgraded oro/crm-magento1-connector to the version that supports doctrine 2.8 - upgraded akeneo/batch-bundle to the version that supports doctrine 2.8 - overrode the service in alice fixtures package to make it compatible with doctrine/persistence as the newer version is not compatible with oro/platform application - dropped dependency to doctrine/doctrine-cache-bundle - replaced usage of nyholm/psr7 with laminas/diactoros in the sensio_framework_extra.psr7.http_message_factory - replaced Doctrine\Common\Util\Inflector with Doctrine\Common\Inflector\Inflector - fixed phpstan configuration to work with doctrine/persistence v2 - fixed behat test to use a custom faker field guesser as the build-in one doesn't work with doctrine/persistence v2 - removed isConnected variable set in PersistentConnection.php as it's not needed anymore - renamed doctrine method getEntityManagerForClass to getManagerForClass, as the first one doesn't exist in a new version - fixed installers to set the object_class field length to 191, according to the updated mapped superclass in the gedmo translatable extension - added the service from the doctrine-bundle to excludes the CheckReferenceCompilerPass, as we don't need to decorate it, and there is no need to override it in code yet - fixed doc example integrity after namespaces update - fixed tests, broken after doctrine upgrade - unskipped API tests that were broken because of the bug in the doctrine that is already fixed in 2.8.1 - fixed wrong return value in isOrphanRemoval method in API tests.

--- src/Oro/Bundle/CookieConsentBundle/Tests/Functional/DataFixtures/LoadVisitorsData.php
+++ src/Oro/Bundle/CookieConsentBundle/Tests/Functional/DataFixtures/LoadVisitorsData.php
@@ -3,7 +3,7 @@
 namespace Oro\Bundle\CookieConsentBundle\Tests\Functional\DataFixtures;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Oro\Bundle\CustomerBundle\Entity\CustomerVisitor;
 
 class LoadVisitorsData extends AbstractFixture
