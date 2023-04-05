3a5e3cf6da573c3584979ae14c359394f25d1a3f
Yurii Muratov <ymuratov@oroinc.com>
2023-04-05 17:50:49 +0300

    BB-22342: Email confirmation link for customer user registration when autologin is enabled (#35255)

--- src/Oro/Bundle/CookieConsentBundle/EventListener/CustomerUserRegistrationAndLoginListener.php
+++ src/Oro/Bundle/CookieConsentBundle/EventListener/CustomerUserRegistrationAndLoginListener.php
@@ -4,29 +4,21 @@ namespace Oro\Bundle\CookieConsentBundle\EventListener;
 
 use Oro\Bundle\CookieConsentBundle\Helper\CookiesAcceptedPropertyHelper;
 use Oro\Bundle\CookieConsentBundle\Helper\FrontendRepresentativeUserHelper;
-use Oro\Bundle\CustomerBundle\CustomerUserEvents;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
 use Oro\Bundle\CustomerBundle\Entity\CustomerVisitor;
 use Oro\Bundle\CustomerBundle\Event\FilterCustomerUserResponseEvent;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Component\Security\Http\SecurityEvents;
 
 /**
  * Transfers "cookies_accepted" parameter if it is set for CustomerVisitor
  * who currently registers
  */
-class CustomerUserRegistrationAndLoginListener implements EventSubscriberInterface
+class CustomerUserRegistrationAndLoginListener
 {
-    /** @var FrontendRepresentativeUserHelper */
-    private $frontendUserHelper;
-
-    /** @var CookiesAcceptedPropertyHelper */
-    private $cookiesAcceptedHelper;
-
-    /** @var DoctrineHelper */
-    private $doctrineHelper;
+    private FrontendRepresentativeUserHelper $frontendUserHelper;
+    private CookiesAcceptedPropertyHelper $cookiesAcceptedHelper;
+    private DoctrineHelper $doctrineHelper;
 
     public function __construct(
         FrontendRepresentativeUserHelper $frontendUserHelper,
@@ -36,36 +28,6 @@ class CustomerUserRegistrationAndLoginListener implements EventSubscriberInterfa
         $this->frontendUserHelper = $frontendUserHelper;
         $this->cookiesAcceptedHelper = $cookiesAcceptedHelper;
         $this->doctrineHelper = $doctrineHelper;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            CustomerUserEvents::REGISTRATION_COMPLETED => 'onRegistrationCompleted',
-            SecurityEvents::INTERACTIVE_LOGIN => 'onSecurityInteractiveLogin'
-        ];
-    }
-
-    /**
-     * @throws \Doctrine\ORM\ORMException
-     */
-    private function transferCookieAccepted(CustomerVisitor $customerVisitor, CustomerUser $customerUser): void
-    {
-        if (false === $this->cookiesAcceptedHelper->isCookiesAccepted($customerVisitor)) {
-            return;
-        }
-
-        if (true === $this->cookiesAcceptedHelper->isCookiesAccepted($customerUser)) {
-            return;
-        }
-
-        $this->cookiesAcceptedHelper->setCookiesAccepted($customerUser, true);
-        $entityManager = $this->doctrineHelper->getEntityManagerForClass(CustomerUser::class);
-        $entityManager->persist($customerUser);
-        $entityManager->flush();
     }
 
     public function onRegistrationCompleted(FilterCustomerUserResponseEvent $event): void
@@ -85,5 +47,21 @@ class CustomerUserRegistrationAndLoginListener implements EventSubscriberInterfa
                 $this->transferCookieAccepted($customerVisitor, $user);
             }
         }
+    }
+
+    private function transferCookieAccepted(CustomerVisitor $customerVisitor, CustomerUser $customerUser): void
+    {
+        if (false === $this->cookiesAcceptedHelper->isCookiesAccepted($customerVisitor)) {
+            return;
+        }
+
+        if (true === $this->cookiesAcceptedHelper->isCookiesAccepted($customerUser)) {
+            return;
+        }
+
+        $this->cookiesAcceptedHelper->setCookiesAccepted($customerUser, true);
+        $entityManager = $this->doctrineHelper->getEntityManagerForClass(CustomerUser::class);
+        $entityManager->persist($customerUser);
+        $entityManager->flush();
     }
 }
