d8c4c932e23891e04b9aea357748baf6ffbb429c
Andrii Yatsenko <ayatsenko@oroinc.com>
2023-09-11 21:55:52 +0300

    BAP-22238: Use Symfony 6 return types (#36389)

--- src/Oro/Bundle/CookieConsentBundle/Form/Type/ConfigLandingPageSelectType.php
+++ src/Oro/Bundle/CookieConsentBundle/Form/Type/ConfigLandingPageSelectType.php
@@ -107,7 +107,7 @@ class ConfigLandingPageSelectType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return self::NAME;
     }
@@ -123,7 +123,7 @@ class ConfigLandingPageSelectType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getParent()
+    public function getParent(): ?string
     {
         return PageSelectType::class;
     }
