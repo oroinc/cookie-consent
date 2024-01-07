fe771170cffe1fbde543d2e9ac17fa7f13d0201e
Vova Soroka <vsoroka@oroinc.com>
2024-01-07 14:34:08 +0200

    BAP-22395: Refactor functional tests fixtures that depend on default organization (#37293)

--- src/Oro/Bundle/CookieConsentBundle/Tests/Functional/ActionGroup/OroCookieConsentSetAcceptedCookiesTest.php
+++ src/Oro/Bundle/CookieConsentBundle/Tests/Functional/ActionGroup/OroCookieConsentSetAcceptedCookiesTest.php
@@ -6,9 +6,9 @@ use Oro\Bundle\CookieConsentBundle\Tests\Functional\DataFixtures\LoadVisitorsDat
 use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
 use Oro\Bundle\CustomerBundle\Security\Token\AnonymousCustomerUserToken;
 use Oro\Bundle\FrontendTestFrameworkBundle\Migrations\Data\ORM\LoadCustomerUserData;
-use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Oro\Bundle\SecurityBundle\Authentication\Token\UsernamePasswordOrganizationToken;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadOrganization;
 
 /**
  * @dbIsolationPerTest
@@ -18,21 +18,24 @@ class OroCookieConsentSetAcceptedCookiesTest extends WebTestCase
     protected function setUp(): void
     {
         $this->initClient();
-        $this->loadFixtures([LoadVisitorsData::class]);
+        $this->loadFixtures([LoadVisitorsData::class, LoadOrganization::class]);
     }
 
     public function testRunActionGroupWithCustomerUserAsUser()
     {
-        $registry = self::getContainer()->get('doctrine');
-        $user = $registry
+        /** @var CustomerUser $user */
+        $user = self::getContainer()->get('doctrine')
             ->getRepository(CustomerUser::class)
             ->findOneBy(['username' => LoadCustomerUserData::AUTH_USER]);
-        $organization = $registry->getRepository(Organization::class)->getFirst();
 
         self::assertFalse($user->getCookiesAccepted());
 
-        $token = new UsernamePasswordOrganizationToken($user, 'key', $organization, $user->getUserRoles());
-        $this->client->getContainer()->get('security.token_storage')->setToken($token);
+        self::getContainer()->get('security.token_storage')->setToken(new UsernamePasswordOrganizationToken(
+            $user,
+            'key',
+            $user->getOrganization(),
+            $user->getUserRoles()
+        ));
 
         $action = $this->client->getContainer()->get('oro_action.action.run_action_group');
         $action->initialize([
@@ -47,11 +50,11 @@ class OroCookieConsentSetAcceptedCookiesTest extends WebTestCase
     {
         $customerVisitor = $this->getReference(LoadVisitorsData::CUSTOMER_VISITOR);
 
-        $registry = self::getContainer()->get('doctrine');
-        $organization = $registry->getRepository(Organization::class)->getFirst();
-
-        $token = new AnonymousCustomerUserToken($customerVisitor, [], $organization);
-        $this->client->getContainer()->get('security.token_storage')->setToken($token);
+        self::getContainer()->get('security.token_storage')->setToken(new AnonymousCustomerUserToken(
+            $customerVisitor,
+            [],
+            $this->getReference(LoadOrganization::ORGANIZATION)
+        ));
 
         $action = $this->client->getContainer()->get('oro_action.action.run_action_group');
         $action->initialize([
